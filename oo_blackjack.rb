f230d168a3dbe7cb75cce84d838ed265ba6b3cfe
Fan-Chen Pai <fanchen.pai@gmail.com>
2013-10-10 23:20:21 -0700

    work in progress, 70% done...

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -2,19 +2,39 @@
 
 
 module Blackjack
-  def calculate_total
-
+  BLACKJACK_NUMBER = 21
+  DEALER_STOP_POINT = 17
+  def calculate_total(arr)
+  total = 0
+  ace_count = 0
+  arr.each do |card|
+    break unless card.instance_of? Card
+    if card.face_value.to_i.to_s == card.face_value
+      total += card.face_value.to_i
+    elsif ['J','Q','K'].include?(card.face_value)
+      total += 10
+    else # 'A'
+      total += 11
+      ace_count += 1
+    end
+  end
+  until ace_count == 0 || total <= BLACKJACK_NUMBER
+    total -= 10
+    ace_count -= 1
+  end
+  total
   end
 
   def busted? (v)
-    v > 21
+    v > BLACKJACK_NUMBER
   end
 
-  def compute_result
+  def compare_result
 
   end
 
 end
+
 
 
 class Card
@@ -30,6 +50,7 @@ class Card
   end
 
 end
+
 
 class Deck
   attr_accessor :cards, :num_decks, :in_order
@@ -55,6 +76,7 @@ class Deck
 
   def deal
     initialize(@num_decks,@in_order) if @cards.empty?
+    puts "Dealing..."
     @cards.pop
   end
 
@@ -64,47 +86,122 @@ class Deck
 
 end
 
+
 class Participant
   include Blackjack
   attr_accessor :name, :cards
 
-  def initialize ; end
+  def initialize(s)
+    @name = s
+    @cards = []
+  end
 
-  def show_cards
+  def show_hand
+    puts "Cards in #{@name}'s hand:"
+    ret = cards.map { |c| c.to_s}
+    puts "#{ret.join(', ')} Total: #{total}"
+  end
 
+  def info
+    "#{self.class}: #{@name}"
+  end
+
+  def total
+    calculate_total(cards)
   end
 
 end
 
-class Dealer < Participant
-  def choose_action
 
+class Dealer < Participant
+  def hit?
+    if calculate_total(@cards) < 17
+      puts "Dealer chose to hit"
+      true
+    end
   end
 
 end
 
 
 class Player < Participant
-  def choose_action
-
+  def hit?
+    puts "\> What do you want to do?"
+    puts "\> 1) Hit  or  2) Stay"
+    input_ok = false
+    until input_ok
+      s = gets.chomp
+      if ['1','2'].include?(s)
+        puts "You chose to #{s=='1' ? 'Hit' : 'Stay'}\n\n"
+        return s == '1'
+      else
+        puts "#{s}\> Please enter either '1' or '2' to continue..."
+      end
+    end
   end
 
 end
+
 
 class CardGame
   include Blackjack
-  attr_accessor :num_players, :players, :deck, :round_count
+  attr_accessor :players, :deck, :round_count
 
-  def ask_name
+  def initialize
+    @players = []
+    @round_count = 0
+    @deck = Deck.new
 
+    players << Player.new(get_user_name)
+    players << Dealer.new("Dealer")
   end
 
-  def round_start
+  def get_user_name
+    puts "\> Hello! What's your name?"
+    3.times do
+      s = gets.chomp
+      if s.strip.length > 0
+        puts "Hi, #{s}"
+        return s
+      end
+    end
+    "Player"
+  end
+
+  def run
+    @round_count += 1
+    puts "******************************"
+    puts "  Blackjack Game - Round #{round_count}"
+    puts "******************************"
+    players.each do |p|
+      p.cards << deck.deal
+      p.cards << deck.deal
+      p.show_hand if p.instance_of? Player
+      while p.hit? do
+        p.cards << deck.deal
+        p.show_hand if p.instance_of? Player
+        # todo - check result
+      end
+
+      # todo - ask if continue
+
+    end
 
   end
 
 end
 
+g = CardGame.new
+g.run
+puts g.players.inspect
 
-d = Deck.new
-60.times {puts d.deal }
+
+# d = Deck.new
+# c_arr = []
+# 3.times { c_arr << d.deal }
+# puts c_arr
+
+# p = Player.new("test")
+# puts p.calculate_total(c_arr)
+
+
